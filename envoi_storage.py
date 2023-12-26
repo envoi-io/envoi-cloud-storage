6f62a321ce6e5768a2df4dcf0e58a7fa4ae2f236
John Whitson <john.whitson@gmail.com>
2023-12-26 14:07:30 -0700

    Added CloudFormation helper

--- envoi_storage.py
+++ envoi_storage.py
@@ -20,6 +20,58 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 
+class AwsCloudFormationHelper:
+
+    @classmethod
+    def client_from_opts(cls, cfn_client_args=None, opts=None):
+        if cfn_client_args is None:
+            cfn_client_args = {}
+
+        if opts is None:
+            opts = SimpleNamespace()
+
+        if opts.aws_profile is not None:
+            cfn_client_args['profile_name'] = opts.aws_profile
+
+        if opts.aws_region is not None:
+            cfn_client_args['region_name'] = opts.aws_region
+
+        return boto3.client('cloudformation', **cfn_client_args)
+
+    @classmethod
+    def create_stack(cls, stack_name, template_url, cfn_role_arn=None, template_parameters=None, client=None,
+                     cfn_client_args=None):
+        if client is None:
+            if cfn_client_args is None:
+                cfn_client_args = {}
+            client = boto3.client('cloudformation', **cfn_client_args)
+
+        cfn_create_stack_args = {
+            'StackName': stack_name,
+            'TemplateURL': template_url
+        }
+
+        if template_parameters is not None:
+            cfn_create_stack_args['Parameters'] = template_parameters
+
+        if cfn_role_arn is not None:
+            cfn_create_stack_args['RoleARN'] = cfn_role_arn
+
+        return client.create_stack(**cfn_create_stack_args)
+
+    @classmethod
+    def populate_template_parameters_from_opts(cls, template_parameters, opts, field_map):
+        for opts_param_name, template_param_name in field_map:
+            value = getattr(opts, opts_param_name, None)
+            if value is not None:
+                template_parameters.append({
+                    "ParameterKey": template_param_name,
+                    "ParameterValue": value
+                })
+
+        return template_parameters
+
+
 class EnvoiArgumentParser(argparse.ArgumentParser):
 
     def to_dict(self):
